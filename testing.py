de31ffded4b1fd149f5199b4210edbd67ba4cc54
ziyad34 <149248314+ziyad34@users.noreply.github.com>
2024-07-08 21:33:11 +0300

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,16 @@
+# Import libraries necessary for this project
+import numpy as np
+import pandas as pd
+from time import time
+from IPython.display import display # Allows the use of display() for DataFrames
+
+# Import supplementary visualization code visuals.py
+import visuals as vs
+
+
+# Load the Census dataset
+data = pd.read_csv("census.csv")
+
+
+n_records = np.data[income].count
+print(n_records)
